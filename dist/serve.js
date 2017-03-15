541b60aa82a5a77b0c6d2ee0c693e75aca83ff13
scratchos <computer.backup.15@gmail.com>
2017-03-15 19:40:39 +0000

    Vue.js

---
+++ dist/serve.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+app.use(express.static(__dirname));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
