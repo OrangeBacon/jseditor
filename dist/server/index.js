9164acb1f7f75203e1de6a83c9b780f879b449bd
scratchos <computer.backup.15@gmail.com>
2017-03-16 16:41:20 +0000

    vuex, server updates

--- dist/server/index.js
+++ dist/server/index.js
@@ -1,6 +1,11 @@
 module.exports =  function serve(app,files=true){
   var express = require('express');
   var path = require('path');
+  
+  app.get('/hello',function(req,res){
+    res.send('hello world');
+  });
+  
   if(files){
     app.use(express.static(path.resolve(__dirname,'..')));
   }
